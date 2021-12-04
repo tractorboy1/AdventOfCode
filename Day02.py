7a45d302981daf0666b6b8bc0756c7863cec60a7
Edward Jones <edjone@cisco.com>
2021-12-04 18:37:34 +0000

    Day 2 done

--- Day02.py
+++ Day02.py
@@ -26,17 +26,44 @@ def parse_instruction(instruction: str) -> Optional[Movement]:
         exit(1)
     return movement
 
-location = CoOrd(x=0,depth=0)
-for line in instructions.split("\n"):
-    movement = parse_instruction(line)
-    if movement.direction == "forward":
-        location.x += movement.distance
-    elif movement.direction == "up":
-        location.depth -= movement.distance
-    elif movement.direction == "down":
-        location.depth += movement.distance
-    else:
-        raise ValueError("Invalid movement direction")
+def movement_part1():
+    location = CoOrd(x=0,depth=0)
+    for line in instructions.split("\n"):
+        movement = parse_instruction(line)
+        if movement.direction == "forward":
+            location.x += movement.distance
+        elif movement.direction == "up":
+            location.depth -= movement.distance
+        elif movement.direction == "down":
+            location.depth += movement.distance
+        else:
+            raise ValueError("Invalid movement direction")
 
-print(f"Depth is {location.depth}, distsance is {location.x}")
-print(f"The product of those is {location.depth * location.x}")
+    print(f"Depth is {location.depth}, distsance is {location.x}")
+    print(f"The product of those is {location.depth * location.x}")
+
+@dataclass
+class CoOrd2():
+    x: int
+    depth: int
+    aim: int
+
+def movement_part2():
+    location = CoOrd2(x=0,depth=0, aim=0)
+    for line in instructions.split("\n"):
+        movement = parse_instruction(line)
+        if movement.direction == "forward":
+            location.x += movement.distance
+            location.depth += movement.distance * location.aim
+        elif movement.direction == "up":
+            location.aim -= movement.distance
+        elif movement.direction == "down":
+            location.aim += movement.distance
+        else:
+            raise ValueError("Invalid movement direction")
+
+    print(f"Depth is {location.depth}, distsance is {location.x}")
+    print(f"The product of those is {location.depth * location.x}")
+
+movement_part1()
+movement_part2()
